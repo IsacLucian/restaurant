1018418ca7724e1c60681ea468dba9ed5f2d94e1
IsacLucian <40325553+IsacLucian@users.noreply.github.com>
2018-09-15 15:02:06 +0300

    add create_user.php

--- create_user.php
+++ create_user.php
@@ -1,3 +1,16 @@
 <?php
-   echo "hello";
-?>
+
+    include_once 'Connection/connect.php';
+
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $sql = "INSERT INTO users(name, email, pass, phone) VALUES('$name', '$email', '$pass', '$phone')";
+
+    if(mysqli_query($conn, $sql) == FALSE){
+        echo "Error: " . mysqli_error($conn);
+    }
+
+    header ("Location: /");
--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php include 'menu.php' ?>
 
 <div class="container">
-	<form>
+	<form action = "create_user.php" method = "post">
 	  <div class="form-group">
 		<label> Name </label>
 	  	<input type="text" class="form-control" name="name" id="name" placeholder="Enter full name">
@@ -12,11 +12,11 @@
 	  </div>
 	  <div class="form-group">
 	    <label for="exampleInputEmail1">Email address</label>
-	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name = "email">
 	  </div>
 	  <div class="form-group">
 	    <label for="exampleInputPassword1">Password</label>
-	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name = "pass">
 	  </div>
 	  <button type="submit" class="btn btn-primary">Submit</button>
 	</form>
